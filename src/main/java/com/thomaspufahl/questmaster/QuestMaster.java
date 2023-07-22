c899a850c89cdc435b5e531d3e3e177e831860bf
Thomas Pufahl <thomas.r.pufahl@gmail.com>
2023-07-22 14:33:34 -0300

    Quest Master

---
+++ src/main/java/com/thomaspufahl/questmaster/QuestMaster.java
@@ -0,0 +1,12 @@
+package com.thomaspufahl.questmaster;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuestMaster extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("@ QUEST MASTER @");
+    }
+
+}
