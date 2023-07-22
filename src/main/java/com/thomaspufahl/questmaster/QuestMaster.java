8b90b9705aee2d3ce243dc03dd254df40e3f4b46
Thomas Pufahl <thomas.r.pufahl@gmail.com>
2023-07-22 15:57:10 -0300

    Use DataFile setup in plugin init

--- src/main/java/com/thomaspufahl/questmaster/QuestMaster.java
+++ src/main/java/com/thomaspufahl/questmaster/QuestMaster.java
@@ -1,5 +1,7 @@
 package com.thomaspufahl.questmaster;
 
+
+import com.thomaspufahl.questmaster.data.DataFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class QuestMaster extends JavaPlugin {
@@ -7,6 +9,6 @@ public final class QuestMaster extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("@ QUEST MASTER @");
+        DataFile.setup(this);
     }
-
 }
